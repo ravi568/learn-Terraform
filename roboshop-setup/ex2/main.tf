4192b0e1be4eeb99421cdc3c997605873d7029bb
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-26 20:24:33 +0530

    story 45- for each for instance map of map

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "frontend" {
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e9e4460e83575a63"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+variable "instances" {
+  default = ["cart", "catalogue", "user", "payment", "shipping"]
+}
+
+output "public-ip" {
+  value = aws_instance.frontend.*.public_ip
+}
