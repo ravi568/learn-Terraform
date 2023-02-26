cf1ae3da2d1600cc105642388e20147777823faf
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-26 18:45:00 +0530

    story 43- S3 bucket storing state file

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-bt71"
+    key    = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0e9e4460e83575a63"]
+
+  tags = {
+    name = "test"
+  }
+}
