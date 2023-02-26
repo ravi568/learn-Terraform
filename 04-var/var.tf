2a81adc8b5a547169d38e8814868209819981e97
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-26 11:08:33 +0530

    story 41 - var type, var,list,map

--- 04-var/var.tf
+++ 04-var/var.tf
@@ -19,3 +19,28 @@ output "cli" {
 }
 
 variable "input" {}
+
+variable "sample5" {
+  default = "Hello"
+}
+
+variable "sample6" {
+  default =  [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample7" {
+  default =  {
+    string = "Hello",
+    number = 100,
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "variable sample5 - ${var.sample5},first value in list- ${var.sample6[0]},boolean value of map- ${var.sample7["boolean"]}"
+}
