9ffe4c6141ba21c697928d3f701e86f0b57471a5
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-27 22:15:36 +0530

    story 47- module null resrc

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["sg-0e9e4460e83575a63"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z04953962FKYDA691J60N"
+  name    = "${var.component}-dev.kalluriravidevops71.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+
+variable "instance_type" {}
